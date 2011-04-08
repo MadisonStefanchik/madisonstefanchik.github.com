d4eb35a6fe2c93320c90d404bc542726e55e45fa
Madison Stefanchik <madisonleigh44@gmail.com>
2011-04-08 12:30:20 -0400

    mo.js

---
+++ javascript/mo.js
@@ -0,0 +1,11 @@
+// mouseovers generated at http://javascript.about.com
+if (document.images) {
+image71a = new Image;
+image71b = new Image;
+image71a.src = 'http:\/\/madisonleighdesigns.com\/images\/triobw.jpg';
+image71b.src = 'http:\/\/madisonleighdesigns.com\/images\/trio.jpg';
+} else {
+image=71a = '';
+image71b = '';
+document.rollimg71 = '';
+}
